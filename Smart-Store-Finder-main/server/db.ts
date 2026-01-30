96511bb5bb6970db91ff8107b475db761c840a86
HaniVarshitha <163728539+VattikutiHanivarshitha@users.noreply.github.com>
2026-01-30 22:24:53 +0530

    Hani123

--- Smart-Store-Finder-main/server/db.ts
+++ Smart-Store-Finder-main/server/db.ts
@@ -1,4 +1,5 @@
 console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "@shared/schema";
@@ -11,5 +12,9 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
 export const db = drizzle(pool, { schema });
